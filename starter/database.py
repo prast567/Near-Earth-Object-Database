013730ff51f6e6d79c3d14460f01754a38e249ba
pk <pk611886@gmail.com>
2020-02-21 20:16:46 +0530

    loaded data to database

--- starter/database.py
+++ starter/database.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from models import OrbitPath, NearEarthObject
 
 
@@ -16,6 +17,9 @@ class NEODatabase(object):
         """
         # TODO: What data structures will be needed to store the NearEarthObjects and OrbitPaths?
         # TODO: Add relevant instance variables for this.
+        self.filename = filename
+        self.orbit_dict = {}
+        self.neo_earth_dict = {}
 
     def load_data(self, filename=None):
         """
@@ -34,5 +38,28 @@ class NEODatabase(object):
 
         # TODO: Load data from csv file.
         # TODO: Where will the data be stored?
+        neo_df = pd.read_csv(filename)
+        temp_dict = {}
+        orbit = []
+        for ind in neo_df.index:
+            for column in neo_df.columns:
+                temp_dict[column] = neo_df[column][ind]
+            try:
 
-        return None
+                near_earth_object = NearEarthObject(**temp_dict)
+                near_earth_object.update_orbits(orbit)
+            except Exception as e:
+                print(e)
+            orbit_path = OrbitPath(**temp_dict)
+
+            temp_dict.clear()
+
+            if neo_df['close_approach_date'][ind] in self.orbit_dict.keys():
+                self.orbit_dict[neo_df['close_approach_date'][ind]].append(near_earth_object)
+
+            else:
+                self.orbit_dict[neo_df['close_approach_date'][ind]] = []
+                self.orbit_dict[neo_df['close_approach_date'][ind]].append(near_earth_object)
+
+            if neo_df['name'][ind] not in self.orbit_dict.keys():
+                self.neo_earth_dict[neo_df['name'][ind]] = orbit_path
