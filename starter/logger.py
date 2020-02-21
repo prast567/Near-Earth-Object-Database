b5930fd39bc8df4f990146d09e03e9e7475a1992
pk <pk611886@gmail.com>
2020-02-21 22:19:47 +0530

    proper logs added

--- starter/logger.py
+++ starter/logger.py
@@ -1,6 +1,11 @@
+import pathlib
+
+root = pathlib.Path(__file__).parent.parent
+path = f'{root}/test_log.txt'
+
 import logging
 logger = logging.getLogger('neo')
-hdlr = logging.FileHandler('log')
+hdlr = logging.FileHandler(path)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
